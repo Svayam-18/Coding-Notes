e3f21ec97845aef5d617cc27f58ac64f1fa052f8
Svayam-18 <svayamgarg@gmail.com>
2026-01-14 11:33:39 +0530

    This is my first python code for the github

---
+++ Python/00_Hello_World.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+# following the traditioon al first program in any language
+
+print("my name is Svayam Garg")
+print("I am learning Python programming.")
+print("I hope to build amazing projects with Python!")
+print("also I am adding this all in my github")
